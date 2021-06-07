d57043660bdf3c8c707863c430464ed1fb8c3f99
Ary <aryclenio.barros@truckpad.com.br>
2021-06-07 17:12:58 -0300

    🔒️ Implementing stripe payment integration

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1IxaTqJ5nU6I1FPdYhacTUBs", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+};
