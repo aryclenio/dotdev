fc191d79dac8b6a6c443b853463e9cfc0e29a7a7
Ary <aryclenio.barros@truckpad.com.br>
2021-06-07 15:07:24 -0300

    ✨ Integrating with Github Oauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
+
+// Client side
+// Server side
+// Static Site generation
 
 interface HomeProps {
   product: {
@@ -35,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IxaTqJ5nU6I1FPdYhacTUBs", {
     expand: ["product"],
   });
@@ -52,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
