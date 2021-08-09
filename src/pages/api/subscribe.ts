8a33010061ff5d7103ab96384901eafbbb0f2677
arycleniobarros <arycleniobarros@gmail.com>
2021-08-09 10:09:48 -0300

    Finishing preview posts

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,5 +1,5 @@
-import { query as q } from "faunadb";
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/client";
 import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
@@ -7,46 +7,39 @@ import { stripe } from "../../services/stripe";
 type User = {
   ref: {
     id: string;
-  }
+  };
   data: {
     stripe_customer_id: string;
-  }
-}
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
+
     const user = await fauna.query<User>(
-      q.Get(
-        q.Match(
-          q.Index('user_by_email'),
-          q.Casefold(session.user.email)
-        )
-      )
-    )
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
-    let customerId = user.data.stripe_customer_id
+    let customerId = user.data.stripe_customer_id;
 
-    if(!customerId){
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    });
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
 
-
-    await fauna.query(
-      q.Update(
-        q.Ref(q.Collection('users'), user.ref.id),
-        {
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
           data: {
             stripe_customer_id: stripeCustomer.id,
-          }
-        }
-      )
-    )
-    customerId = stripeCustomer.id
-      }
+          },
+        })
+      );
 
-    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customerId = stripeCustomer.id;
+    }
+
+    const StripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
@@ -57,8 +50,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return res.status(200).json({sessionId: stripeCheckoutSession.id})
-  
+    return res.status(200).json({ sessionId: StripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
