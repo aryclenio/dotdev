f8d6a2c9341bae4b47f0c6758003c0cf74bf4b6a
arycleniobarros <arycleniobarros@gmail.com>
2021-08-09 09:45:37 -0300

    ⚡️ Implementing preview post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { SignInButton } from "../SignInButton";
-import styles from "./styles.module.scss";
+import { SignInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -7,13 +8,18 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">
-            Home
-          </a>
-          <a href="">Posts</a>
+
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
     </header>
-  );
+  )
 }
+
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
-import { query } from "faunadb";
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
+
 import Providers from "next-auth/providers";
+
 import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
@@ -12,26 +15,52 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
+
       try {
         await fauna.query(
-          query.If(
-            query.Not(
-              query.Exists(
-                query.Match(
-                  query.Index("user_by_email"),
-                  query.Casefold(user.email)
-                )
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
               )
             ),
-            query.Create(query.Collection("users"), { data: { email } }),
-            query.Get(
-              query.Match(
-                query.Index("user_by_email"),
-                query.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
         );
         return true;
