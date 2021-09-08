3ea0a4513fd46faeaa8cb707b7c54db2f6c091d8
arycleniobarros <arycleniobarros@gmail.com>
2021-09-08 15:36:33 -0300

    Updating posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,5 @@
 
 import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/client"
 import Head from "next/head"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../services/prismic"
@@ -37,22 +36,10 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req })
   const { slug } = params;
-  console.log(String(slug))
-
-  if (!session?.activeSubscription) {
-    return {
-      redirect: {
-        destination: `/posts/preview/${slug}`,
-        permanent: false,
-      }
-    }
-  }
 
   const prismic = getPrismicClient(req)
   const response = await prismic.getByUID('publication', String(slug), {})
-
   const post = {
     slug,
     title: RichText.asText(response.data.title),
